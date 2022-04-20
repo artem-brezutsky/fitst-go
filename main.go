4ba1f2cabec6a416e65f7f3ad5396e0e99b95ab5
artem-brezutsky <a.brezutsky@gmail.com>
2022-04-20 22:52:28 +0300

    commit

--- main.go
+++ main.go
@@ -12,7 +12,12 @@ type Article struct {
 }
 
 var posts []Article
-var templates = []string{"templates/index.html", "templates/header.html", "templates/footer.html"}
+var templates = []string{
+	"templates/index.html",
+	"templates/header.html",
+	"templates/footer.html",
+	"templates/create.html",
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles(templates...)
@@ -24,8 +29,29 @@ func index(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "index", nil)
 }
 
+func create(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles(templates...)
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "create", nil)
+}
+
+func saveArticle(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	fullText := r.FormValue("full_text")
+
+	fmt.Println(title, anons, fullText)
+}
+
 func handleFunc() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	http.HandleFunc("/", index)
+	http.HandleFunc("/create", create)
+	http.HandleFunc("/save_article", saveArticle)
 	http.ListenAndServe(":8080", nil)
 }
 
