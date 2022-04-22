0515dd6f2fb521d1db65021033b69e6eabb9e648
artem-brezutsky <a.brezutsky@gmail.com>
2022-04-22 22:44:51 +0300

    final

--- main.go
+++ main.go
@@ -50,6 +50,24 @@ func index(w http.ResponseWriter, r *http.Request) {
 		posts = append(posts, post)
 	}
 
+	var i int = 10
+	switch i {
+	case 0:
+		fmt.Println("Zero")
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	case 3:
+		fmt.Println("Three")
+	case 4:
+		fmt.Println("Four")
+	case 5:
+		fmt.Println("Five")
+	default:
+		fmt.Println("Unknown Number")
+	}
+
 	defer db.Close()
 
 	t.ExecuteTemplate(w, "index", posts)
